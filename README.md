e7d6250064e58bfefc3d7964756a03f39bd30c79
Dan Herbert <status-report-dan@hrbrt.co>
2025-09-14 08:57:38 -0400

    Add basic status report
    Early version of report generation using shell scripting instead of the
    eventual Python script that generates a text file to be parsed by my
    tmux-status-right script in my dotfiles to surface system problems when
    actively signed into a server.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Status Report Generator
+
+Alpha-version work-in-progress
+
+Current exists as a shell script & systemd polling timer, but will eventually be
+a configurable Python service.
---
+++ basic-report/status-report.sh
@@ -0,0 +1,55 @@
+#!/bin/bash -e
+
+if [[ $EUID != 0 ]]; then
+    echo 'Script must be run as root'
+    exit 1
+fi
+
+primary_user_state_home="/home/dan/.local/state"
+app_state_path="$primary_user_state_home/dotfiles"
+status_path="$app_state_path/system_state"
+mail_path="$app_state_path/mail_state"
+mapfile -t user_names < <(loginctl list-users | tail -n+2 | head -n-2 | grep 'yes' | awk '{ print $2 }')
+declare -A previous_states
+if [[ -e "$status_path" ]] && (( $(wc -l < "$status_path") > 0 )); then
+    while IFS="" read -r line || [ -n "$line" ]; do
+        user=$(echo "$line" | awk '{ print $2 }')
+        state=$(echo "$line" | awk '{ print $3 }')
+        timestamp=$(echo "$line" | awk '{ print $4 }')
+        previous_states["$user"]="$state $timestamp"
+    done < "$status_path"
+fi
+
+truncate -s 0 "$status_path"
+
+system_state="$(systemctl show --value --property=SystemState)"
+previous_state_details="${previous_states['root']}"
+prev_state=$(echo "$previous_state_details" | awk '{ print $1 }')
+prev_timestamp=$(echo "$previous_state_details" | awk '{ print $2 }')
+timestamp=$(date -u +%s)
+if [[ $system_state == "$prev_state" ]] && [[ -n "$prev_timestamp" ]]; then
+    timestamp=$prev_timestamp
+fi
+echo "system_state root $system_state $timestamp" >> "$status_path"
+
+for uname in "${user_names[@]}"; do
+    user_system_state="$(systemctl --machine="$uname"@ --user show --value --property=SystemState)"
+    previous_state_details="${previous_states[$uname]}"
+    prev_state=$(echo "$previous_state_details" | awk '{ print $1 }')
+    prev_timestamp=$(echo "$previous_state_details" | awk '{ print $2 }')
+    timestamp=$(date -u +%s)
+    if [[ $system_state == "$prev_state" ]] && [[ -n "$prev_timestamp" ]]; then
+        timestamp=$(echo "$previous_state_details" | awk '{ print $2 }')
+    fi
+    echo "system_state $uname $user_system_state $timestamp" >> "$status_path"
+done
+
+truncate -s 0 "$mail_path"
+if mail -e >/dev/null 2>&1; then
+    echo "mailbox_unread root yes" >> "$mail_path"
+fi
+for uname in "${user_names[@]}"; do
+    if [[ -e "/var/mail/$uname" ]] || [[ -e "/var/spool/mail/$uname" ]]; then
+        echo "mailbox_unread $uname yes" >> "$mail_path"
+    fi
+done
